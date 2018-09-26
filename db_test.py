b92ceb572e474a5c32dc90dc74ee2cf9c95319f0
James <j1459.j1459@gmail.com>
2018-09-27 01:17:28 +0800

    attribution and remove unused code

--- db_test.py
+++ db_test.py
@@ -30,37 +30,6 @@ import db_test_config
 
 
 
-
-### https://stackoverflow.com/questions/4613465/using-python-to-write-mysql-query-to-csv-need-to-show-field-names
-##import MySQLdb as dbapi
-##import sys
-##import csv
-##
-##
-##dbQuery='SELECT * FROM `asagi experiments 2018-7`.a;'
-##
-##db=dbapi.connect(
-##    host=db_test_config.IP,
-##    user=db_test_config.USERNAME,
-##    passwd=db_test_config.PASSWORD
-##)
-##
-##cur=db.cursor()
-##cur.execute(dbQuery)
-##
-##result = cur.fetchall()
-##
-###Getting Field Header names
-##column_names = [i[0] for i in cur.description]
-##fp = open('Result_Set.csv', 'w')
-##myFile = csv.writer(fp, lineterminator = '\n') #use lineterminator for windows
-##myFile.writerow(column_names)
-##myFile.writerows(result)
-##fp.close()
-
-
-
-
 def dev():
     """For development/debugging in IDE/editor without CLI arguments"""
     logging.warning('running dev()')
@@ -95,75 +64,6 @@ if __name__ == '__main__':
     logging.info("Program finished.")
 
 
-##
-##
-### https://www.blog.pythonlibrary.org/2010/09/10/sqlalchemy-connecting-to-pre-existing-databases/
-### Connect to the DB
-##from sqlalchemy import create_engine, MetaData, Table
-##from sqlalchemy.orm import mapper, sessionmaker
-##import sqlalchemy
-##
-##class BoardClass(object):
-##    """Represents a board we are running over"""
-##    pass
-##
-##
-##table_name = 'a'
-##
-### Load session
-##engine = create_engine(db_test_config.CONNECT_STRING, echo=True)
-##
-##metadata = MetaData(engine)
-##table = Table(table_name, metadata, autoload=True)
-##mapper(BoardClass, table)
-##
-##Session = sessionmaker(bind=engine)
-##session = Session()
-##
-### Get column names
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-### Get table columns
-##
-##
-##
-### Prepare row 0 of CSV from columns
-##
-##
-### Read table
-##result = session.query(BoardClass).all()
-####    print(res[1].timestamp)
-### Iterate over rows and dump them to CSV
-##
-##
-##
-##
-##
-##
-##
-##metadata.bind = engine
-##
-##mytable = sqlalchemy.Table('a', metadata, autoload=True)
-##db_connection = engine.connect()
-##
-##select = sqlalchemy.sql.select([mytable])
-##result = db_connection.execute(select)
-##
-##fh = open('data.csv', 'wb')
-##outcsv = csv.writer(fh)
-##
-##outcsv.writerow(result.keys())
-####outcsv.writerows(result)
-##
-##fh.close
 
 
 
@@ -173,13 +73,14 @@ if __name__ == '__main__':
 
 
 
+# https://stackoverflow.com/questions/2952366/dump-csv-from-sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
 Base = automap_base()
 
-engine = create_engine(db_test_config.SQLITE_CONNECT_STRING, echo=True)
+engine = create_engine(db_test_config.SQLITE_CONNECT_STRING, echo=True)# https://docs.sqlalchemy.org/en/latest/core/engines.html#sqlite
 
 Base.prepare(engine, reflect=True)
 
